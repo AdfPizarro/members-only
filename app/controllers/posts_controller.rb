f1b3699682c678476ef55f88ff44eb7d60a0051f
Adrian <adfpizarro@gmail.com>
2020-08-11 11:42:16 -0600

    Update posts table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:title, :content)
     end
 end
